c25bd5cbef2615f30acecd74c4666b70dd254f25
lsy223622 <lsy223622@outlook.com>
2026-03-18 14:27:54 +0800

    Bump app versionName to 1.1.0
    Update Android app versionName in app/build.gradle.kts from 1.0.0 to 1.1.0 to reflect the new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lsy223622.motionphotomanager"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
