8e18dbb7b2d8aaf9609a31c2d1babd0b71b9b823
lsy223622 <lsy223622@outlook.com>
2026-03-22 00:40:34 +0800

    Bump app version to 1.3.0 (code 5)
    Update versionCode from 4 to 5 and versionName from "1.2.0" to "1.3.0" in app/build.gradle.kts to prepare the next app release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lsy223622.motionphotomanager"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
